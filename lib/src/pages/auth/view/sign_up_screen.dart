1210177fe4ccaf60407c885522f22ab93ef4bc6a
João Gabriel <jgmoura.dev@gmail.com>
2022-12-04 15:51:37 -0300

    validando campo de recuperação de senha

--- lib/src/pages/auth/view/sign_up_screen.dart
+++ lib/src/pages/auth/view/sign_up_screen.dart
@@ -123,9 +123,11 @@ class SignUpScreen extends StatelessWidget {
                                   onPressed: authController.isLoading.value
                                       ? null
                                       : () {
+                                          FocusScope.of(context).unfocus();
                                           if (_formKey.currentState!
                                               .validate()) {
                                             _formKey.currentState!.save();
+
                                             authController.signUp();
                                           }
                                         },
--- lib/src/pages/common_widgets/custom_text_field.dart
+++ lib/src/pages/common_widgets/custom_text_field.dart
@@ -12,6 +12,7 @@ class CustomTextField extends StatefulWidget {
   final void Function(String?)? onSaved;
   final TextEditingController? controller;
   final TextInputType? textInputType;
+  final GlobalKey<FormFieldState>? formFieldKey;
 
   const CustomTextField(
       {Key? key,
@@ -24,7 +25,8 @@ class CustomTextField extends StatefulWidget {
       this.validator,
       this.onSaved,
       this.controller,
-      this.textInputType})
+      this.textInputType,
+      this.formFieldKey})
       : super(key: key);
 
   @override
@@ -46,6 +48,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15),
       child: TextFormField(
+        key: widget.formFieldKey,
         controller: widget.controller,
         readOnly: widget.readOnly,
         initialValue: widget.initialValue,
