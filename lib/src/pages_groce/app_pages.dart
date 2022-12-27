8d9bd08440e622cacbad687ced0afa499bf74190
João Gabriel <jgmoura.dev@gmail.com>
2022-12-27 10:42:53 -0300

    Recebendo dados por argumentos do GetX

--- lib/src/pages_groce/app_pages.dart
+++ lib/src/pages_groce/app_pages.dart
@@ -5,10 +5,12 @@ import 'package:greengrocer/src/pages/base/base_screen.dart';
 import 'package:greengrocer/src/pages/base/binding/navigation_biding.dart';
 import 'package:greengrocer/src/pages/cart/binding/cart_binding.dart';
 import 'package:greengrocer/src/pages/home/bilding/home_blinding.dart';
+import 'package:greengrocer/src/pages/product/product_screen.dart';
 import 'package:greengrocer/src/pages/splash/splash_screen.dart';
 
 abstract class AppPages {
   static final pages = <GetPage>[
+    GetPage(name: PagesRoutes.productRoutes, page: () => ProductScreen()),
     GetPage(name: PagesRoutes.splashScreen, page: () => const SplashScreen()),
     GetPage(name: PagesRoutes.signInRoutes, page: () => SignInScreen()),
     GetPage(name: PagesRoutes.signUp, page: () => SignUpScreen()),
@@ -22,6 +24,7 @@ abstract class AppPages {
 
 abstract class PagesRoutes {
   static const String signInRoutes = '/signIn';
+  static const String productRoutes = '/products';
   static const String signUp = '/signup';
   static const String splashScreen = '/splash';
   static const String baseScrenn = '/';
